b1b37aa2e245dcdd0b4965fcc9eefef5fb466a4e
NeonMight <srh10a@acu.edu>
2015-09-25 17:55:20 -0500

    Partials are still being rendered uncompiled. Working on writing a directive

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,12 +1,18 @@
+// this app should follow a single controller approach, or at least a single app approach
 var ngapp = angular.module('main', ['ngSanitize']);
 
 ngapp.controller('mainController', ['$scope', '$http', '$sce', function($scope, $http, $sce)
 {
   //console.log('Controller is active');
 
+  // SCOPE DATA MEMBERS
   $scope.session = '';
+  $scope.currentPage = '';
+  $scope.userProfile = null;
 
-  var getSession = function(){
+
+  // SCOPE ACTIONS
+  $scope.getSession = function(){
     // request get user session
     $http.get('/getUserSession/').success(function(response){
       // insert extra links if admin and add name to greeting on navbar
@@ -15,17 +21,58 @@ ngapp.controller('mainController', ['$scope', '$http', '$sce', function($scope, 
     });
   };
 
-  $scope.currentPage = '';
-
   $scope.getPage = function(url){
     $http.get(url).success(function(response){
       //console.log(response);
       $scope.currentPage = $sce.trustAsHtml(response);
       // now tell it to get resources for that app
+      $scope.getResources(url);
     });
   };
 
-  getSession();
+  $scope.getResources = function(url){
+    //depending on page loaded, get the resources for that page
+    console.log("Requesting resources for "+url);
+    if (url=='/profile/')
+    {
+      console.log('Getting profile...');
+      $scope.getUserProfile()
+    };
+  };
+
+  // SPECIAL CASES
+  $scope.getUserProfile = function()
+  {
+    $http.get('/getUserProfile/').success(function(response)
+    {
+      $scope.userProfile = response;
+    });
+  };
+
+  $scope.updateUserProfile = function()
+  {
+    $http.put('/updateProfile/',$scope.userProfile).success(function(response)
+    {
+      //do something
+      refresh();
+      //alert('Your account has been updated successfully');
+      $('#submissionComplete').modal();
+    });
+  }
+
+
+  // Load resources and templates
+  $scope.getSession();
   $scope.getPage('/main/');
 
 }]);
+
+ngapp.directive('bindy', function($compile) {
+  return {
+    link: function($scope, $element, $attrs) {
+      var html = $scope.$eval($attrs.bindy);
+      $element.html(html);
+      $compile($element.contents())($scope);
+    }
+  };
+});
